d0635f22b2dffb0f056fb23abd274b6c442a39e7
speelgren <97473666+speelgren@users.noreply.github.com>
2024-01-07 22:52:07 +0100

    Update RestBackend.java

--- laboration-4/mflix_backend/src/mongo/mflix/RestBackend.java
+++ laboration-4/mflix_backend/src/mongo/mflix/RestBackend.java
@@ -55,7 +55,7 @@ public class RestBackend {
 		  logger.info(prop.getProperty("db.name"));
 
 		  /* TITLE */
-	      get("/title/:title", (req,res)-> {
+	      get("/title/:title", (req, res)-> {
 	    	  
 		      MongoCollection<Document> collection = database.getCollection("movies");
 		      String filter=req.params("title").toLowerCase();
@@ -68,7 +68,8 @@ public class RestBackend {
 	    	      myDoc.remove("poster");
 	    	      myDoc.remove("fullplot");
 	    	      myDoc.remove("cast");
-	    	      
+
+		    	  res.status(200);
 	    	      return myDoc.toJson();
 	    	  } else {
 	    		  res.status(404);
@@ -77,7 +78,7 @@ public class RestBackend {
 	      });
 	      
 	      /* FULLPLOT */
-	      get("/fullplot/:title", (req,res)-> {
+	      get("/fullplot/:title", (req, res)-> {
 	    	  
 		      MongoCollection<Document> collection = database.getCollection("movies");
 		      String filter=req.params("title").toLowerCase();
@@ -89,6 +90,7 @@ public class RestBackend {
 	    		  String title = myDoc.getString("title");
 	    		  String fullplot = myDoc.getString("fullplot");
 
+		    	  res.status(200);
 		          return new Document("title", title).append("fullplot", fullplot).toJson();
 	    	  } else {
 	    		  res.status(404);
@@ -97,7 +99,7 @@ public class RestBackend {
 	      });
 
 	      /* CAST */
-	      get("/cast/:title", (req,res)-> {
+	      get("/cast/:title", (req, res)-> {
 	    	  
 	    	  MongoCollection<Document> collection = database.getCollection("movies");
 		      String filter=req.params("title").toLowerCase();
@@ -109,24 +111,62 @@ public class RestBackend {
 	    		  String title = myDoc.getString("title");
 	    		  List<String > cast = myDoc.getList("cast", String.class);
 
+		    	  res.status(200);
 		          return new Document("title", title).append("cast", cast).toJson();
 	    	  } else {
 	    		  res.status(404);
 	    		  return ("<html><body><h1>Title not found.</h1></body></html>");
 	    	  }
+	    	  
 	      });
 	      
 	      /* GENRE */
-	      get("/genre/:genre", (req,res)-> {
+	      get("/genre/:genre", (req, res) -> {
+
+	    	    MongoCollection<Document> collection = database.getCollection("movies");
+	    	    String filter = req.params("genre").toLowerCase();
+	    	    filter = WordUtils.capitalizeFully(filter);
+	    	    logger.info("Finding genre: " + filter);
+
+	    	    AggregateIterable<Document> myDoc = collection.aggregate(Arrays.asList(
+	    	        Aggregates.match(Filters.eq("genres", filter)),
+	    	        Aggregates.project(Projections.fields(
+	    	        		Projections.exclude("_id", "poster", "fullplot", "cast")
+	    	        )),
+	    	        Aggregates.sort(Sorts.ascending("title")),
+	    	        Aggregates.limit(10)
+	    	    ));
+
+	    	    List<Document> movies = new ArrayList<>();
+	    	    try (MongoCursor<Document> iterator = myDoc.iterator()) {
+
+	    	        if (!iterator.hasNext()) {
+	    	            res.status(404);
+	    	            return ("<html><body><h1>Title not found.</h1></body></html>");
+	    	        }
+
+	    	        while (iterator.hasNext()) {
+	    	            movies.add(iterator.next());
+	    	        }
+	    	    }
+
+	    	    res.type("application/json");
+		    	res.status(200);
+	    	    return new Gson().toJson(movies);
+	    	});
+
+	      
+	      /* ACTOR */
+	      get("/actor/:cast", (req, res) -> {
 	    	  
 	    	  MongoCollection<Document> collection = database.getCollection("movies");
-		      String filter=req.params("genre").toLowerCase();
+		      String filter=req.params("cast").toLowerCase();
 		      filter=WordUtils.capitalizeFully(filter);
-		      logger.info("Finding genre: " + filter);
+		      logger.info("Finding actor: " + filter);
 		      
 		      AggregateIterable<Document> myDoc = collection.aggregate(Arrays.asList(
-		    		  Aggregates.match(Filters.eq("genres", filter)),
-		    		  Aggregates.project(Projections.include("title", "genres")),
+		    		  Aggregates.match(Filters.eq("cast", filter)),
+		    		  Aggregates.project(Projections.include("title", "cast")),
 		    		  Aggregates.sort(Sorts.ascending("title")),
 		    		  Aggregates.limit(10)
 		    		  ));
@@ -145,48 +185,29 @@ public class RestBackend {
 		      }
 		      
 		      res.type("application/json");
+	    	  res.status(200);
 		      return new Gson().toJson(movies);
 	      });
 	      
-	      /* ACTOR */
-	      get("/fullplot/:title", (req,res)-> {
+	      /* POST Title */
+	      post("/title", (req, res) -> {
 	    	  
-		      MongoCollection<Document> collection = database.getCollection("movies");
-		      logger.info("Finding all different cuisines ");
-	    	  DistinctIterable<String> col = collection.distinct("plot", String.class);
-	    	  JsonArray list = new JsonArray();
-	    	  JsonObject myDoc = new JsonObject();
-	    	  
-	    	  if (col != null) {
-	    		  for (String tmp : col) { 
-	    			  list.add(tmp);	 
-	    		  }
-	    	  myDoc.add("plot", list);	  
-	    	  } else {
-	    		  res.status(404);
-	    		  return ("<html><body><h1>No plot found.</h1></body></html>");
-	    	  }
-	    	  
-	          return myDoc;
-	      });
-	      
-	      /*
-	      post("/title", (request, response) -> {
-	    	  
-	    	  response.type("application/json");
 	    	  try {
 	    	    	
 	    		  MongoCollection<Document> collection = database.getCollection("movies");
-	    		  collection.insertOne(new Document(Document.parse(request.body())));
+	    		  collection.insertOne(new Document(Document.parse(req.body())));
 	    		  logger.info("Adding Titles ");
 	    		   
 	            } catch (MongoException me) {
 	              System.err.println("Unable to insert due to an error: " + me);
+	              
+	              res.status(500);
 	            }
-	    	    
-	    	   response.status(202);
+
+	    	   res.type("application/json");
+	    	   res.status(202);
 	    	   return ("<html><body><h1>Titles Accepted.</h1></body></html>");
-	      });*/
+	      });
 	}
 
 }
